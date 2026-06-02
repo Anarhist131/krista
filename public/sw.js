eb68b40146be365364a47d9bef1eccde3296b98d
ЛесГем Company <Kukharev600@gmail.com>
2026-06-02 15:11:24 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,22 @@
-self.addEventListener('fetch', (event) => {
+const CACHE_NAME = 'kmessenger-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+  // можно добавить другие ресурсы, если нужно
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request);
+    })
   );
 });
