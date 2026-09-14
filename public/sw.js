dac8291a0c044a01de917529f7eed49694aa81b6
ЛесГем Company <Kukharev600@gmail.com>
2026-09-14 21:05:21 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'krista-v1.20';
+const CACHE = 'krista-v1.24.0';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/o.mp3'];
 
 self.addEventListener('install', (e) => {
